2f90ee139660ec605c9489face26081b4c93d810
Jonathan Gomez <jongomezdev@gmail.com>
2021-06-15 12:23:40 -0400

    selection sort es15

--- selectionSort.js
+++ selectionSort.js
@@ -22,11 +22,34 @@ function selectionSort(arr) {
         lowest = j;
       }
     }
-    var temp = arr[i];
-    arr[i] = arr[lowest];
-    arr[lowest] = temp;
+    if (i !== lowest) {
+      var temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
   }
   return arr;
 }
 
-selectionSort([2, 3, 4, 5, 34, 22, 10, 19, 17]);
+selectionSort([34, 22, 10, 19, 17]);
+
+// ES2015
+
+function selectionSort(arr) {
+  const swap = (arr, idx1, idx2) => [
+    arr[idx1],
+    (arr[idx2] = arr[idx2]),
+    arr[idx1],
+  ];
+
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[lowest] > arr[j]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) swap(arr, i, lowest);
+  }
+  return arr;
+}
