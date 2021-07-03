07f3f19797c472fb209cacf0720e2f940bfecb85
Jonathan Gomez <jongomezdev@gmail.com>
2021-07-02 23:40:36 -0400

    revisit class syntax for data structures

---
+++ classSyntax.js
@@ -0,0 +1,53 @@
+class Student {
+  constructor(firstName, lastName, year) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.grade = year;
+    this.tardies = 0;
+    this.scores = [];
+  }
+  fullName() {
+    return `Your full name is ${this.firstName} ${this.lastName}`;
+  }
+  markLate() {
+    this.tardies += 1;
+    if (this.tardies >= 3) {
+      return 'YOU ARE EXPELLED!!!!';
+    }
+    return `${this.firstName} ${this.lastName} has been late ${this.tardies} times`;
+  }
+  addScore(score) {
+    this.scores.push(score);
+    return this.scores;
+  }
+  calculateAverage() {
+    let sum = this.score.reduce(function (a, b) {
+      return a + b;
+    });
+    return sum / this.scores.length;
+  }
+  static EnrollStudents() {
+    return 'Enrolling Students';
+  }
+}
+
+let firstStudent = new Student('Jonathan', 'Gomez', 3);
+
+//firstStudent.fullName();
+
+// MDN Docs
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  static distance(a, b) {
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
+    return Math.hypot(dx, dy);
+  }
+}
+
+const p1 = new Point(5, 5);
+const p2 = new Point(10, 10);
--- codility.js
+++ codility.js
@@ -27,3 +27,31 @@ function solution(A) {
   }
   return x;
 }
+
+/**
+ * There is a road consisting of N segments, numbered from 0 to N-1,
+ * represented by a string S. Segment S[K]
+ * of the road may contain a pothole, denoted by a single uppercase "x" character,
+ * or may be a good segment without any potholes, denoted by a single dot, ".".
+ *
+ * For example, string ".X..X" means that there are two potholes in total in the road:
+ * one is located in segment S[1] and one in segment S[4]. All other segments are good.
+ *
+ * The road fixing machine can patch over three consecutive segments at once with
+ * asphalt and repair all the potholes located within each of these segments. Good or
+ * already repaired segments remain good after patching them.
+ * segments remain good after patching them.
+ *
+ * Your task is to compute the minimum number of patches required to repair all
+ * the potholes in the road.
+ *
+ * Write a function: funciton solution(A) that, given a string S of length N, returns
+ * the minimum number of patches required to repair all the potholes.
+ *
+ * Examples:
+ *
+ * 1. Given S = ".X..X", your function should return 2. The road fixing machine could,
+ * patch for example, segments 0-2 and 2-4.
+ *
+ * 2.
+ */
