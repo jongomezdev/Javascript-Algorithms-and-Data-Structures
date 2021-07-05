3b7864e1dede56758cd6a73876eb51c107d141d1
Jonathan Gomez <jongomezdev@gmail.com>
2021-07-05 12:38:05 -0400

    remove method

--- linkedList.js
+++ linkedList.js
@@ -94,6 +94,18 @@
  */
 
 /**
+ * Remove Pseudocode
+ * - If the index is less than zero or greater than the length
+ * return undefined.
+ * - If the index is the same as the length -1, pop
+ * - If the index is 0, shift
+ * - Otherwise, using the get method, access the node at the index -1
+ * - Set the next property on that node to be the next of the next node
+ * - Decrement the length
+ * - Return the value of the node removed
+ */
+
+/**
  * To be able to traverse our list we start at the begining and loop
  * while there is a .next
  * - create a traverse method
@@ -205,6 +217,16 @@ class SinglyLinkedList {
     newNode.next = temp;
     this.length++;
     return true;
+  }
+  remove(index) {
+    if (index < 0 || index > this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    var previousNode = this.get(index - 1);
+    var removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length--;
+    return removed;
   }
 }
 
